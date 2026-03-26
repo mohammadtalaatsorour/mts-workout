5f70247216fa956051bcb1d2cf773e7c0bdd18e8
mohammadtalaatsorour <mohammadtalaatsorour@gmail.com>
2026-03-26 19:46:40 +0000

    modified:   index.html 	modified:   sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Caches all assets for full offline support
    ═══════════════════════════════════════════════════ */
 
-const CACHE_NAME  = 'mts-workout-v9';
+const CACHE_NAME  = 'mts-workout-v10';
 const CACHED_URLS = [
   './index.html',
   './manifest.json',
